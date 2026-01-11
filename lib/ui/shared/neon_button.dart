b713301e048c8ccdbdebd64bae9a2b0636257fae
adrisabik <adrisabik@gmail.com>
2026-01-11 19:11:15 +0700

    arch(ui): add placeholder NeonButton shared widget

---
+++ lib/ui/shared/neon_button.dart
@@ -0,0 +1,23 @@
+// Placeholder for neon button widget
+// To be implemented in Sprint 4.3
+
+// import 'package:flutter/material.dart';
+// import 'package:chroma_switch/core/theme/app_colors.dart';
+
+// class NeonButton extends StatelessWidget {
+//   final String text;
+//   final VoidCallback onTap;
+//   final Color color;
+//   
+//   const NeonButton({
+//     super.key,
+//     required this.text,
+//     required this.onTap,
+//     this.color = AppColors.cyan,
+//   });
+//   
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
