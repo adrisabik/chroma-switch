82398885316c9aac7641a1fe5f1c0932040fd73d
adrisabik <adrisabik@gmail.com>
2026-01-11 19:10:22 +0700

    arch(game): add placeholder PlayerBall component

---
+++ lib/game/components/player_ball.dart
@@ -0,0 +1,14 @@
+// Placeholder for PlayerBall component
+// To be implemented in Sprint 1.3
+
+// import 'package:flame/components.dart';
+// import 'package:flame/collisions.dart';
+// import 'package:chroma_switch/game/chroma_game.dart';
+
+// class PlayerBall extends PositionComponent 
+//     with HasGameRef<ChromaGame>, CollisionCallbacks {
+//   Color currentColor;
+//   late Vector2 velocity;
+//   
+//   PlayerBall({Color? initialColor});
+// }
