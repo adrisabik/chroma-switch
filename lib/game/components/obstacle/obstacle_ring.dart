9c8a214f272bd0259a443abb382d0abc00062ff9
adrisabik <adrisabik@gmail.com>
2026-01-11 19:10:37 +0700

    arch(game): add placeholder ObstacleRing and RingSegment components

---
+++ lib/game/components/obstacle/obstacle_ring.dart
@@ -0,0 +1,16 @@
+// Placeholder for ObstacleRing component
+// To be implemented in Sprint 2.2
+
+// import 'package:flame/components.dart';
+// import 'package:chroma_switch/game/chroma_game.dart';
+
+// class ObstacleRing extends PositionComponent 
+//     with HasGameRef<ChromaGame> {
+//   final double rotationSpeed;
+//   
+//   ObstacleRing({this.rotationSpeed = 1.0});
+//   
+//   void reset() {
+//     // Reset for object pooling
+//   }
+// }
---
+++ lib/game/components/obstacle/ring_segment.dart
@@ -0,0 +1,19 @@
+// Placeholder for RingSegment component
+// To be implemented in Sprint 2.2
+
+// import 'package:flame/components.dart';
+// import 'package:flame/collisions.dart';
+// import 'package:chroma_switch/game/chroma_game.dart';
+
+// class RingSegment extends PositionComponent 
+//     with HasGameRef<ChromaGame>, CollisionCallbacks {
+//   final Color color;
+//   final double startAngle;
+//   final double sweepAngle;
+//   
+//   RingSegment({
+//     required this.color,
+//     required this.startAngle,
+//     this.sweepAngle = 3.14159 / 2,
+//   });
+// }
