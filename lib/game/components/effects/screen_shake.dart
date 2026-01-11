ff936ecfec07fa2351053d261e089e89c4a88f17
adrisabik <adrisabik@gmail.com>
2026-01-11 19:10:44 +0700

    arch(game): add placeholder explosion and screen shake effects

---
+++ lib/game/components/effects/screen_shake.dart
@@ -0,0 +1,12 @@
+// Placeholder for ScreenShake effect
+// To be implemented in Sprint 4.1
+
+// import 'package:flame/components.dart';
+// import 'package:chroma_switch/game/chroma_game.dart';
+
+// class ScreenShake extends Component
+//     with HasGameRef<ChromaGame> {
+//   void trigger({double? intensity, double? duration}) {
+//     // Trigger screen shake effect
+//   }
+// }
