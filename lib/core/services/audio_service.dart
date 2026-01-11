61d1671aed0200ae3dfb263371f188924f806130
adrisabik <adrisabik@gmail.com>
2026-01-11 19:11:02 +0700

    arch(core): add placeholder AudioService and StorageService

---
+++ lib/core/services/audio_service.dart
@@ -0,0 +1,18 @@
+// Placeholder for AudioService
+// To be implemented in Sprint 4.2
+
+// import 'package:flame_audio/flame_audio.dart';
+
+// class AudioService {
+//   void playSfx(String filename) {
+//     // Play sound effect
+//   }
+//   
+//   void playBgm(String filename) {
+//     // Play background music
+//   }
+//   
+//   void stopBgm() {
+//     // Stop background music
+//   }
+// }
